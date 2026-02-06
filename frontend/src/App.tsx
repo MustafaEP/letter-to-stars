a96d15fdf786ed1344f4ca5355b5d8a1f08ad56b
MustafaEP <portakalm12@gmail.com>
2026-02-06 14:33:00 +0300

    feat: initializze React + Tailwind frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold">
+        ⭐ Letter to Stars
+      </h1>
+    </div>
+  )
+}
+
+export default App
