ef7e57ab4b0bf3a9ca7d264149693289ac8e48d5
MustafaEP <portakalm12@gmail.com>
2026-02-07 18:18:40 +0300

    chore: clarify webhook startup log as secure

--- webhook/index.js
+++ webhook/index.js
@@ -49,5 +49,5 @@ app.post("/deploy", (req, res) => {
 });
 
 app.listen(9000, () => {
-    console.log("- Webhook listener running on port 9000");
+    console.log("- Secure webhook listener running on port 9000");
 })
